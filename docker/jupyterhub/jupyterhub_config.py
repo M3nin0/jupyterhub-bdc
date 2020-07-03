3bf3a558a732ea62d39322b88166fab1cf161db2
Vitor Gomes <vconrado@gmail.com>
2020-07-03 11:47:08 -0300

    adding support do user volume

--- docker/jupyterhub/jupyterhub_config.py
+++ docker/jupyterhub/jupyterhub_config.py
@@ -10,7 +10,15 @@ c.NotebookApp.allow_origin = '*'
 # https://oauthenticator.readthedocs.io/en/latest/getting-started.html#gitlab-setup
 # http://tljh.jupyter.org/en/latest/howto/auth/google.html
 from oauthenticator.google import LocalGoogleOAuthenticator
-c.JupyterHub.authenticator_class = LocalGoogleOAuthenticator
+
+
+# Create a new oauthenticator just to normalize the username
+class BDCLocalGoogleOAuthenticator(LocalGoogleOAuthenticator):
+    def normalize_username(self, username):
+        return username.replace("@", "_at_")
+
+
+c.JupyterHub.authenticator_class = BDCLocalGoogleOAuthenticator
 
 c.LocalGoogleOAuthenticator.client_id = os.environ['GOOGLE_CLIENTID']
 c.LocalGoogleOAuthenticator.client_secret = os.environ['GOOGLE_CLIENT_SECRET']
@@ -29,6 +37,9 @@ c.JupyterHub.hub_ip = os.environ['HUB_IP']
 ## user data persistence
 ## see https://github.com/jupyterhub/dockerspawner#data-persistence-and-dockerspawner
 notebook_dir = os.environ.get('DOCKER_NOTEBOOK_DIR') or '/home/jovyan'
+users_base_dir_source = os.environ.get('USERS_BASE_DIR_SOURCE')
+users_base_dir_target = os.environ.get('USERS_BASE_DIR_TARGET')
+
 c.DockerSpawner.notebook_dir = notebook_dir
 ## ToDo: Define persistence per user
 # c.DockerSpawner.volumes = { "jupyterhub-user-{username}": notebook_dir }
@@ -42,6 +53,17 @@ c.DockerSpawner.volumes = {
     examples_source : {"bind": examples_target, "mode": "ro"}, 
     data_source: {"bind": data_target, "mode": "ro"}
 }
+
+def create_dir_hook(spawner):
+    username = spawner.user.name  # get the username
+    volume_path = os.path.join(users_base_dir_target, username)
+    if not os.path.exists(volume_path):
+        os.mkdir(volume_path, 0o755)
+    os.chown(volume_path, 1000, 100)
+    spawner.volumes[os.path.join(users_base_dir_source, username)] = { "bind": '/home/jovyan/work'}
+
+# attach the hook function to the spawner
+c.Spawner.pre_spawn_hook = create_dir_hook
 
 # Server usage
 c.Spawner.cpu_limit = 1
