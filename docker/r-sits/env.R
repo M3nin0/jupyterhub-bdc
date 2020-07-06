f7a6693026aa60d8c42718342c50df89fdf68ac7
Felipe Menino <felipe.carlos@fatec.sp.gov.br>
2020-07-06 17:42:41 -0300

    Update r-sits Dockerfile

--- docker/r-sits/env.R
+++ docker/r-sits/env.R
@@ -1,4 +1,9 @@
 require('devtools')
 
+# manual installation
+chooseCRANmirror(graphics=FALSE, ind=1)
+install.packages('rgdal', type = "source", 
+                    configure.args=c('--with-proj-include=/usr/local/include', '--with-proj-lib=/usr/local/lib'))
+
 devtools::install_github('e-sensing/sits')
 devtools::install_github("e-sensing/inSitu")
